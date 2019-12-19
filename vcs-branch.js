2bfee46ba1d22bd326450cc5243d29ec00519c70
philipsz-davido <philipsz-davido>
2019-12-19 20:27:06 +0100

    Added some features

--- vcs-branch.js
+++ vcs-branch.js
@@ -4,7 +4,7 @@ const path = require("path")
 
 
 function runBranchCommand(args) {
-
+    // create a new branch
 }
 
 module.exports = runBranchCommand
---
+++ vcs-checkout.js
@@ -0,0 +1,10 @@
+const l = console.log
+const fs = require("fs")
+const path = require("path")
+
+
+function runCheckoutCommand(args) {
+    // create a new branch
+}
+
+module.exports = runCheckoutCommand
